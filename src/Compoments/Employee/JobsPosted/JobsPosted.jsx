8cdc963dc289d04b5acf32a549895123d32a6dea
Guidein7 <atem.learning@gmail.com>
2024-08-09 16:57:11 +0530

    Update JobsPosted.jsx

--- src/Compoments/Employee/JobsPosted/JobsPosted.jsx
+++ src/Compoments/Employee/JobsPosted/JobsPosted.jsx
@@ -588,8 +588,8 @@ function JobsPosted() {
                                         value={formData.companyName}
                                         onChange={handleChange}
                                         className='mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3'
-            
-                                        required
+                                         readOnly
+                                         required
                                     />
                                     {isTouched.companyName && errors.companyName && <span style={{ color: 'red' }}>{errors.companyName}</span>}
 
