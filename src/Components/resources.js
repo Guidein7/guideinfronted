769965a728509a92f5e257e86b01cf4b17b80732
unknown <saikumartanakala2001@gmail.com>
2025-06-29 00:02:48 +0530

    resources.js

--- src/Components/resources.js
+++ src/Components/resources.js
@@ -1,6 +1,6 @@
-const myIp = `192.168.184.1:9000/`
+const myIp = `https://guidein-backend-guidein-backend.lomaem.easypanel.host/`
 // const myIp = `localhost:8080`
 
 export const resources  = {
-    APPLICATION_URL : `http://${myIp}guidein/api/v1/`
+    APPLICATION_URL : `${myIp}guidein/api/v1/`
 }
