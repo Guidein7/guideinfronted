a3450747d760376cd5b360266dabb18663ea633f
veerababu13 <devalankaveerababu@gmail.com>
2025-09-07 23:54:33 +0530

    changes in resources

--- src/Components/resources.js
+++ src/Components/resources.js
@@ -1,7 +1,6 @@
-const myIp = `https://guidein-backend-guidein-backend-test.lomaem.easypanel.host/`
+const myIp = `https://guidein-backend-guidein-backend.lomaem.easypanel.host/`
 // const myIp = `localhost:8080`
 
 export const resources  = {
     APPLICATION_URL : `${myIp}guidein/api/v1/`
 }
-  
